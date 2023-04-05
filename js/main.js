c229cfcd2c02644871d7035f3bb4cb1170572cf7
luizfspinto <luizfspintoo@gmail.com>
2023-04-05 14:45:59 -0300

    update page index

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ function showSlides() {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 5500); // Tempo de troca dos sliders
 }
 
 
 //Evento de Scroll
-window.onscroll = function() {
+window.onscroll = function () {
   myScrolll();
 }
 
