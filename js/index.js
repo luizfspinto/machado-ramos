a2b506566a761450d7b1887095cc85f9755f896f
luizfspinto <luizfspintoo@gmail.com>
2023-04-05 12:31:12 -0300

    going up the site

---
+++ js/index.js
@@ -0,0 +1,4 @@
+//Evento de tempo, após alguns segundos a página é redirecionado para outra
+setTimeout(function() {
+  window.location.href = "home.html";
+}, 4200);
---
+++ js/main.js
@@ -0,0 +1,28 @@
+let slideIndex = 0;
+showSlides();
+
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5500); // Tempo de troca dos sliders
+}
+
+
+//Evento de Scroll
+window.onscroll = function() {
+  myScrolll();
+}
+
+function myScrolll() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
